9f5595ea54c5af07f6eb971da9ca6fe6e301119b
Pen Peen <ikegami1022.h@gmail.com>
2025-04-15 22:51:35 +0900

    feat(layout): レイアウト, スタイル微修正

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/globals.css";
-
+import PublicHeader from "@/components/layouts/PublicHeader";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <PublicHeader />
         {children}
       </body>
     </html>
