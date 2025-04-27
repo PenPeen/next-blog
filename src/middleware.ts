5c537b4e4a6aca9b26e7686384cad74602be9971
Pen Peen <ikegami1022.h@gmail.com>
2025-04-28 08:17:53 +0900

    feat(middleware): パスへのアクセス制御追加

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse, type NextRequest } from 'next/server';
+import { getCurrentUser } from './app/(auth)/fetcher';
+
+const protectedRoutes = ['/account'];
+
+function isProtectedRoute(path: string): boolean {
+  return protectedRoutes.some(route => {
+    return path === route || path.startsWith(`${route}/`);
+  });
+}
+
+export async function middleware(request: NextRequest) {
+  const path = new URL(request.url).pathname;
+
+  if (isProtectedRoute(path)) {
+    const user = await getCurrentUser(request);
+
+    if (!user) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/((?!.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'
+  ],
+};
