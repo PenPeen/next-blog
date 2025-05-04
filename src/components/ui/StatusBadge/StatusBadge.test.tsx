11e5c4ac5b1dddf34d84a01b470fc306bc321609
Pen Peen <ikegami1022.h@gmail.com>
2025-05-04 13:00:22 +0900

    fix(tests): StatusBadgeのインポートパスを修正

--- src/components/ui/StatusBadge/StatusBadge.test.tsx
+++ src/components/ui/StatusBadge/StatusBadge.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { StatusBadge } from "../posts/StatusBadge";
+import { StatusBadge } from "@/components/ui/StatusBadge";
 
 describe('StatusBadge', () => {
   describe('初期表示', () => {
