b300c1db80649a9614ccbeba48f53fd8f54677f8
Pen Peen <ikegami1022.h@gmail.com>
2025-05-11 16:36:17 +0900

    feat(fetcher): GraphQLクエリをDocument形式に変更し、コードを簡素化

--- src/fetcher/getPublishedPosts.ts
+++ src/fetcher/getPublishedPosts.ts
@@ -1,28 +1,9 @@
-import { apolloClient } from "@/app/graphql";
+import { apolloClient, GetPostsDocument } from "@/app/graphql";
 import { cache } from "react";
-import { gql } from "@apollo/client";
-import { POSTS_FRAGMENT } from "@/components/ui/Posts";
-
-export const GET_POSTS_QUERY = gql`
-  ${POSTS_FRAGMENT}
-  query FetchPublishedPosts($page: Int, $perPage: Int) {
-    posts(page: $page, perPage: $perPage) {
-      posts {
-        ...postsTopFragment
-      }
-      pagination {
-        totalCount
-        limitValue
-        totalPages
-        currentPage
-      }
-    }
-  }
-`;
 
 export const getPosts = cache(async (page = 1, perPage = 15) => {
   const { data } = await apolloClient.query({
-    query: GET_POSTS_QUERY,
+    query: GetPostsDocument,
     variables: { page, perPage },
   });
   return { json: () => Promise.resolve(data.posts) };
