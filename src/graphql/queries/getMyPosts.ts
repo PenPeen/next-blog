b4b9d9e93cb795becff459ec6b9afe602bae4200
Pen Peen <ikegami1022.h@gmail.com>
2025-05-20 11:00:02 +0900

    refactor(MyPostItem): フラグメント名はより具象的な命名とする。

--- src/graphql/queries/getMyPosts.ts
+++ src/graphql/queries/getMyPosts.ts
@@ -1,12 +1,12 @@
-import { MY_POST_FRAGMENT } from '@/components/ui/MyPostItem';
+import { MY_POST_ITEM_FRAGMENT } from '@/components/ui/MyPostItem';
 import { gql } from '@apollo/client';
 
 export const GET_MY_POSTS = gql`
   query GetMyPosts($page: Int, $perPage: Int) {
-    ${MY_POST_FRAGMENT}
+    ${MY_POST_ITEM_FRAGMENT}
     myPosts(page: $page, perPage: $perPage) {
       posts {
-        ...MyPost
+        ...MyPostItem
       }
       pagination {
         totalCount
