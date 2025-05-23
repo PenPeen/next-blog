ba1b6515baf646ce44b69a772ac6174af7dbab4d
Pen Peen <ikegami1022.h@gmail.com>
2025-05-23 21:11:19 +0900

    feat(getMyPost): MyPostFormのフラグメントを更新

--- src/graphql/queries/getMyPostFrom.ts
+++ src/graphql/queries/getMyPostFrom.ts
@@ -1,11 +1,11 @@
 import { gql } from '@apollo/client';
-import { MY_POST_FORM_FRAGMENT } from '@/components/ui/MyPostForm';
+import { POST_FORM_FRAGMENT } from '@/components/ui/PostForm';
 
 export const GET_MY_POST_FROM = gql`
   query myPost($id: ID!) {
-    ${MY_POST_FORM_FRAGMENT}
+    ${POST_FORM_FRAGMENT}
     myPost(id: $id) {
-      ...MY_POST_FORM_FRAGMENT
+      ...POST_FORM_FRAGMENT
     }
   }
 `;
