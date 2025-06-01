f6c54597f6c0c56b6e42c9fd777fa05d2fcbda40
Pen Peen <ikegami1022.h@gmail.com>
2025-06-01 11:06:14 +0900

    refactor(EditPostForm): 投稿更新処理をリファクタリング

--- src/components/ui/EditPostForm/index.tsx
+++ src/components/ui/EditPostForm/index.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import React, { useState } from 'react';
-import { makeClient } from '@/app/ApolloWrapper';
-import { UpdatePostDocument } from '@/app/graphql/generated';
 import { useRouter } from 'next/navigation';
-import PostForm, { PostFormData } from '@/components/ui/PostForm';
+import PostForm from '@/components/ui/PostForm';
 import DeletePostForm from '@/components/ui/DeletePostForm';
 import styles from './EditPostForm.module.css';
+import { PostFormData } from '@/lib/schema/post';
+import { updatePost } from '@/mutations/updatePost';
 
 type PostType = {
   id: string;
@@ -32,46 +32,15 @@ export default function EditPostForm({ post }: EditPostFormProps) {
     setErrorMessage('');
 
     try {
-      const client = makeClient();
+      const responseData = await updatePost(post.id, data);
 
-      let thumbnail = undefined;
-      if (data.thumbnail && data.thumbnail.length > 0) {
-        const file = data.thumbnail[0];
-        const reader = new FileReader();
-        thumbnail = await new Promise<string>((resolve) => {
-          reader.onloadend = () => {
-            const base64 = reader.result as string;
-            resolve(base64);
-          };
-          reader.readAsDataURL(file);
-        });
+      if (responseData.errors) {
+        setErrorMessage(responseData.errors.map((error: { message: string }) => error.message).join('\n'));
+      } else if (responseData.post) {
+        setMessage('投稿を更新しました');
+        router.refresh();
       }
-
-      const { data: responseData } = await client.mutate({
-        mutation: UpdatePostDocument,
-        variables: {
-          input: {
-            postInput: {
-              id: post.id,
-              title: data.title,
-              content: data.content,
-              published: data.status === 'published',
-              thumbnail
-            }
-          }
-        }
-      });
-
-      if (responseData?.updatePost?.errors) {
-        setErrorMessage(responseData.updatePost.errors.map((error: { message: string }) => error.message).join('\n'));
-      } else {
-        if (responseData?.updatePost?.post) {
-          setMessage('投稿を更新しました');
-          router.refresh();
-        }
-      }
-    } catch (error) {
-      console.error('更新エラー:', error);
+    } catch {
       setErrorMessage('更新中にエラーが発生しました。しばらく経ってから再度試してください。');
     } finally {
       setIsSubmitting(false);
